ff8d3e4aaa636fe56a1f1871e1be087a8334bf37
Cristian Morales <cr15moresp@gmail.com>
2021-04-27 11:25:07 -0500

    Se cambio el color del statusBar

--- lib/src/pages/zapato_page.dart
+++ lib/src/pages/zapato_page.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'package:shoesapp/src/helpers/helpers.dart';
+
 import 'package:shoesapp/src/widgets/custom_widgets.dart';
 
 class ZapatoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    cambiarStatusLight();
+
     return SafeArea(
       child: Scaffold(
         body: Column(
